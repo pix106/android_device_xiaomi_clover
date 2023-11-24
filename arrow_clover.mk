d17a437b4aae0ae9546454090b44009e3c0bfc36
pix106 <sbordenave@gmail.com>
2023-11-24 07:41:10 +0100

    clover: remove targets which are already in common

--- arrow_clover.mk
+++ arrow_clover.mk
@@ -21,10 +21,6 @@
 # definition file).
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit some common Arrow stuff
 $(call inherit-product, vendor/arrow/config/common.mk)
 
