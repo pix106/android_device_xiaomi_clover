c39bff506206e983058f536b6d17a5c4651bf774
pix106 <sbordenave@gmail.com>
2023-12-24 10:05:16 +0100

    clover: Pixelbuilds time

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -21,11 +21,11 @@
 # definition file).
 #
 
-# inherit MI PAD 4 ArrowOS device config
+# inherit MI PAD 4 AOSP device config
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_clover.mk
+    $(LOCAL_DIR)/aosp_clover.mk
 
 COMMON_LUNCH_CHOICES := \
-		arrow_clover-eng \
-		arrow_clover-userdebug \
-		arrow_clover-user
+		aosp_clover-eng \
+		aosp_clover-userdebug \
+		aosp_clover-user
