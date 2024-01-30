09dbf166df00494d2759b9faaed7eb63abba77cf
Darth9 <reyprimerjefe@gmail.com>
2024-01-30 18:55:44 +0000

    Upgrade the magisk addon to v26.4

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -34,6 +34,7 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v26.4.zip
 	export FOX_ENABLE_APP_MANAGER=1
 	export FOX_USE_BASH_SHELL=1
 	export FOX_ASH_IS_BASH=1
